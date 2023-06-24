e4435d34262e8d34fcf32551a69a7d15b6a597d0
Elizabeth Quinn <brousy2222@gmail.com>
2023-06-24 08:28:32 -0400

    Javascript update

---
+++ assets/JS/script.js
@@ -0,0 +1,12 @@
+// buttons
+
+// questions and answers elements
+
+// The array questions for the quiz game
+
+// Time is subtratcted from clock with wrong answer
+
+
+// When all questions are answered or time reaches 0 the gane is over
+
+// When game is over I can save my initials and score
